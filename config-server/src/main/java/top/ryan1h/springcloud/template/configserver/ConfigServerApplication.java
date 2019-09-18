4038960a66db5edfc2de80c1c7a7619643438e24
ryan1h <599418899@qq.com>
2019-09-19 00:11:44 +0800

    配置中心通过eureka注册

--- config-server/src/main/java/top/ryan1h/springcloud/template/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/top/ryan1h/springcloud/template/configserver/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package top.ryan1h.springcloud.template.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * @date 2019/7/31 13:43
  */
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
