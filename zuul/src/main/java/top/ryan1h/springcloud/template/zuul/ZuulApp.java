1c3b57ecdf8717dd1825c23b5e0bb53d973acc38
ryan1h <599418899@qq.com>
2019-10-20 19:06:09 +0800

    zuul添加oauth2

--- zuul/src/main/java/top/ryan1h/springcloud/template/zuul/ZuulApp.java
+++ zuul/src/main/java/top/ryan1h/springcloud/template/zuul/ZuulApp.java
@@ -2,6 +2,7 @@ package top.ryan1h.springcloud.template.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @EnableZuulProxy
 @SpringBootApplication
+@EnableOAuth2Sso
 public class ZuulApp {
     public static void main(String[] args) {
         SpringApplication.run(ZuulApp.class, args);
